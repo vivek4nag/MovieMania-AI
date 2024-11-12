9e1f55191c53998b9b8664e8ae7916344f06677d
viek4nag <vivek4nag@gmail.com>
2024-11-13 00:50:49 +0530

    router created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <div className="text-3xl font-bold text-red-600">
-      hello world this is my project
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
