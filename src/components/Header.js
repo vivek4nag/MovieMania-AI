debf394d112c6b3e756069d1b919d27049e5292b
viek4nag <vivek4nag@gmail.com>
2024-12-03 22:22:12 +0530

    subtle header changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -93,7 +93,7 @@ const Header = () => {
             onClick={handleGPTSearchClick}
             className="px-2 py-2 text-white font-bold rounded-lg bg-green-600 shadow-md transition-all duration-300 hover:bg-green-900"
           >
-            {showGptSearch ? "GO back" : "GPT Search"}
+            {showGptSearch ? "GO back" : "AI Movie Search"}
           </button>
 
           <button
