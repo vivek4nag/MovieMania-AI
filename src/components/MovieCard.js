1f3c1d431c3a7453df8f88bbdba2a7de0a78f624
viek4nag <vivek4nag@gmail.com>
2024-12-05 15:35:20 +0530

    fallback movie poster added

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { POSTER_IMG_CDN_URL } from "../utils/constants";
+import { POSTER_IMG_CDN_URL, EMPTY_MOVIE_POSTER } from "../utils/constants";
 
-const MovieCard = ({ posterPath }) => {
+const MovieCard = ({ posterPath, movieName }) => {
   return (
     <div className="w-48  flex-shrink-0 rounded-lg overflow-hidden shadow-lg transform hover:scale-95 transition-transform duration-400 cursor-pointer">
-      <img src={POSTER_IMG_CDN_URL + posterPath} alt="poster card" className="w-full h-72 object-cover" />
+      <img src={posterPath? POSTER_IMG_CDN_URL + posterPath : EMPTY_MOVIE_POSTER} alt="poster card" className="w-full h-72 object-cover relative" />
+      {movieName && (
+        <div className="absolute bottom-0 w-full bg-black bg-opacity-80 text-white text-center py-3 text-lg rounded-md">
+
+          <h4 className="text-sm font-semibold">{movieName}</h4>
+        </div>
+      )}
     </div>
   );
 };
